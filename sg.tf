cdeb5f34df11204530913699263cd013386d3ff3
mvpc-11232 <mvp2612@hotmail.com>
2022-11-07 06:46:46 +0530

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "allow_app" {
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEAFULT_VPC_CIDR]
   }
 
+  ingress {
+    description      = "Application Port"
+    from_port        = var.APP_PORT
+    to_port          = var.APP_PORT
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEAFULT_VPC_CIDR]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
--- vars.tf
+++ vars.tf
@@ -3,3 +3,5 @@ variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {} 
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
+variable "APP_PORT" {}
+
