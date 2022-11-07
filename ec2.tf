385c73b538ff77f2136d28148de6990f4432ba00
mvpc-11232 <mvp2612@hotmail.com>
2022-11-07 06:30:33 +0530

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "spot" {
   ami                       = data.aws_ami.my_ami.id
-  instance_type             = "t3.micro"
+  instance_type             = "
   wait_for_fulfillment      = true 
 #   vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 
@@ -11,18 +11,18 @@ resource "aws_spot_instance_request" "spot" {
 
 
 
-  provisioner "remote-exec" {
+# provisioner "remote-exec" {
   
-  # Connection Provisioner
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
+#   # Connection Provisioner
+#   connection {
+#     type     = "ssh"
+#     user     = "centos"
+#     password = "DevOps321"
+#     host     = self.public_ip
+#   }
 
-    inline = [
-      "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
-    ]
-  }
+#     inline = [
+#       "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+#     ]
+#   }
 
