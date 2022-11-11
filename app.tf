3aa0876fb358fc72e7fa7940b5a1bbf0f5fe8001
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:11:46 +0530

    Auto Push Commit

--- app.tf
+++ app.tf
@@ -3,14 +3,14 @@ resource "null_resource" {
 provisioner "remote-exec" {  
   connection {
     type     = "ssh"
-    user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["MYSQL_USERNAME"]
-    password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["MYSQL_USERNAME"]
+    user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+    password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
     host     = element(local.ALL_INSTANCE_PRIVATE_IPS, count.index)
   }
 
     inline = [
-        "exit 1" 
-      // "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+
+      "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
         ]
     }
 }
