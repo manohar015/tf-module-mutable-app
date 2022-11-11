fbfc50690446b401a4d753bbf655b05c23d97035
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 07:02:26 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -6,9 +6,9 @@ resource "aws_lb_target_group" "app" {
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
 
-
 # Enroll Instances to the above target group 
 resource "aws_lb_target_group_attachment" "attach-instance" {
+  count            = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
   target_group_arn = aws_lb_target_group.app.arn
   target_id        = element(local.ALL_INSTANCE_IDS, count.index)
   port             = 8080
