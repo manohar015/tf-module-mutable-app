e62155da240a3b5d63a13110f9e5a6477549fac7
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:56:18 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -1,6 +1,10 @@
+# Creates Target Group 
 resource "aws_lb_target_group" "tg" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = 
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
+
+
+# Enroll Instances to the above target group 
