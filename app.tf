a2bf6f566b0cd6ecc61428cb84b19e8b2c68cc35
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:03:32 +0530

    Auto Push Commit

---
+++ app.tf
@@ -0,0 +1,13 @@
+  provisioner "remote-exec" {
+  
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+    inline = [
+      "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+    ]
+  }
