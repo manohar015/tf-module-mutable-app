1d52c7ad9a834128e8eca2d42c04b864af179e3a
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:57:47 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -1,5 +1,5 @@
 # Creates Target Group 
-resource "aws_lb_target_group" "tg" {
+resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
   port     = 8080
   protocol = "HTTP"
@@ -8,3 +8,8 @@ resource "aws_lb_target_group" "tg" {
 
 
 # Enroll Instances to the above target group 
+resource "aws_lb_target_group_attachment" "attach-instance" {
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = element(local.ALL_INSTANCE_IDS}
+  port             = 80
+}
