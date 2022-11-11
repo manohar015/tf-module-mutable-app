72197986044a1113d0999d31fe936251052f7fcf
mvpc-11232 <mvp2612@hotmail.com>
2022-11-11 06:55:06 +0530

    Auto Push Commit

--- target-group.tf
+++ target-group.tf
@@ -1,6 +1,6 @@
-resource "aws_lb_target_group" "application" {
-  name     = "tf-example-lb-tg"
-  port     = 80
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.COMPONENT}-${var.ENV}"
+  port     = 8080
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = 
 }
