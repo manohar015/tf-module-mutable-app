7fae58529bb8a313663df928e18c9855e95a68d8
mvpc-11232 <mvp2612@hotmail.com>
2022-11-12 06:38:53 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -3,5 +3,9 @@ resource "aws_route53_record" "record" {
   name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS]
+  records = var.LB_TYPE  [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS]
 }
+
+
+# LB_TYPE = internal : internal.zone
+# LB_TYPE = external : external.zone
