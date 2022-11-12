0034cdf9eefc8b3a9800223e480c89a2641c2e72
mvpc-11232 <mvp2612@hotmail.com>
2022-11-12 06:34:22 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,11 +1,7 @@
 resource "aws_route53_record" "record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "${var.COMPONENT}-dev.roboshop.internal"
+  name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ADDRESS}"
   type    = "CNAME"
   ttl     = 10
   records = [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS]
-
 }
-
-
-internal-roboshop-private-316722189.us-east-1.elb.amazonaws.com   --->   cart-dev.roboshop.internal 
