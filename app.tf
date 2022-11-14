a18696c70d25a10263db2e9a4a5c77b0f9094aff
mvpc-11232 <mvp2612@hotmail.com>
2022-11-14 06:56:27 +0530

    Auto Push Commit

--- app.tf
+++ app.tf
@@ -14,7 +14,7 @@ resource "null_resource" "application_deploy" {
 
         inline = [
 
-        "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e MONGO_URL=${data.terraform_remote_state.db.outouts.MONGODB_ENDPOINT}  roboshop-pull.yml"
+        "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e MONGO_URL=${data.terraform_remote_state.db.outputs.MONGODB_ENDPOINT}  roboshop-pull.yml"
             ]
         }
     }
