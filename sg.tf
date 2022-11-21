d4fb0a54db69654be16e87dd4d1001c8bf61fd22
mvpc-11232 <mvp2612@hotmail.com>
2022-11-21 06:29:40 +0530

    Performed some test readme comments

--- sg.tf
+++ sg.tf
@@ -21,9 +21,9 @@ resource "aws_security_group" "allow_app" {
   }
 
   ingress {
-    description      = "SSH Port"
-    from_port        = 22
-    to_port          = 22
+    description      = "Prometheus"
+    from_port        = 9100
+    to_port          = 9100
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
