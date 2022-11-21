29f4385932acf1ae8183d76ed15227731f6254a5
mvpc-11232 <mvp2612@hotmail.com>
2022-11-21 06:29:00 +0530

    Auto Push Commit

--- sg.tf
+++ sg.tf
@@ -20,6 +20,14 @@ resource "aws_security_group" "allow_app" {
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    description      = "SSH Port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
