b99fc7cac8670576f782bac604b9c17bb08b2e82
Manohar <manohar015@gmail.com>
2022-12-20 20:22:33 +0530

    Added my Comments

---
+++ app.tf
@@ -0,0 +1,30 @@
+resource "null_resource" "application_deploy" {
+    triggers = {
+        # versionn = var.APP_VERSION
+          timestamp = timestamp()
+    }
+    count       = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+    provisioner "remote-exec" {  
+    connection {
+        type     = "ssh"
+        user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+        password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
+        host     = element(local.ALL_INSTANCE_PRIVATE_IPS, count.index)
+    }
+
+        inline = [
+                "exit 1"
+        // "ansible-pull -U https://github.com/manohar015/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} -e MONGO_URL=${data.terraform_remote_state.db.outputs.MONGODB_ENDPOINT}  roboshop-pull.yml"
+            ]
+        }
+    }
+
+
+# Note: Provisioners by default are create time provisioners, which means only during the creation they will run. When you run it again for the second time, they won't run. But here in our case, it's an application deployer, which has to run irrespective of the success/failure/partial state of the previous run.
+
+# The timestamp() function in the interpolation syntax will return an ISO 8601 formatted string, which looks like this 2019-02-06T23:22:28Z .
+
+# Rolling Update :  
+#  1.0  2.0 3.0 
+
+#  1.1  2.2 3.3
