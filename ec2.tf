d58e26ca8d0de887fa97a17528756e419deab37a
Manohar <manohar015@gmail.com>
2022-12-20 10:37:23 +0530

    Added my Comments

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment      = true 
   vpc_security_group_ids    = [aws_security_group.allow_app.id]
   subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
-  //iam_instance_profile      = "b50-admin"
+  iam_instance_profile      = "b-50-allow-all"
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
